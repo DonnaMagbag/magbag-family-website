85744ea8f59475f976569fa7c04cc4558c159b8d
DMagbag <donna.magbag@holtonixsolutions.com>
2025-09-26 16:54:32 -0400

    Initial commit

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Magbag Family Website</h1>
+      <p>Start building your app here.</p>
+    </div>
+  );
+}
+
+export default App;
+
+
